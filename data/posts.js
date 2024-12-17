895db2d7b64a9c3f5b982ad6e6ab4294980933af
RobertoFloris <florisroberto99@gmail.com>
2024-12-17 16:22:29 +0100

    [add] posts.js

---
+++ data/posts.js
@@ -0,0 +1,36 @@
+const posts = [
+  {
+      titolo: "Ciambellone",
+      contenuto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+      immagine: "images/ciambellone.jpeg",
+      tags: ["dolci", "ciambellone", "colazione", "ricette tradizionali"]
+  },
+  {
+      titolo: "Cracker alla barbabietola",
+      contenuto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+      immagine: "images/cracker_barbabietola.jpeg",
+      tags: ["snack", "barbabietola", "salutare", "ricette vegane"]
+  },
+  {
+      titolo: "Pane fritto dolce",
+      contenuto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+      immagine: "images/pane_fritto_dolce.jpeg",
+      tags: ["dolci", "pane fritto", "tradizione", "facile"]
+  },
+  {
+      titolo: "Pasta alla barbabietola cremosa",
+      contenuto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+      immagine: "images/pasta_barbabietola.jpeg",
+      tags: ["pasta", "barbabietola", "primi piatti", "originale"]
+  },
+  {
+      titolo: "Torta paesana della tradizione",
+      contenuto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+      immagine: "images/torta_paesana.jpeg",
+      tags: ["dolci", "tradizione", "torta paesana", "ricette casalinghe"]
+  }
+];
+
+
+
+module.exports = posts;
